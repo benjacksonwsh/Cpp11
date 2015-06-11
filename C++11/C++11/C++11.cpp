396388011c31c44697bd980eda6ac93cb13da750
ben <ben.jackson.wsh@gmail.com>
2015-06-11 17:04:42 +0800

    function code

--- C++11/C++11/C++11.cpp
+++ C++11/C++11/C++11.cpp
@@ -4,7 +4,9 @@
 #include "stdafx.h"
 #include <stdint.h>
 #include <Windows.h>
+#include <tchar.h>
 #include <type_traits>
+#include <string>
 
 __pragma(pack(push, 4))
 union Color
@@ -20,26 +22,44 @@ union Color
 
 Color operator"" _argb(const char* agrb) {
 	Color color;
-	char* endPtr = nullptr;
-	color.value = strtol(agrb, NULL, 0);
+	
+	color.value = strtoul(agrb, NULL, 0);
 	return color;
 }
 
-Color operator"" _argb(const char* agrb, std::size_t size) {
+Color operator"" _argb(const char* argb, std::size_t size) {
 	Color color;
 	char* endPtr = nullptr;
-	color.value = strtol(agrb, NULL, 0);
+	
+	std::string strColor(argb,size);
+	
+	int signPos = strColor.find('#');
+	if (signPos >= 0)
+	{
+		strColor.replace(signPos, 1, "0x");
+	}
+
+	color.value = strtoul(strColor.c_str(), NULL, 0);
 	return color;
 }
 __pragma(pack(pop))
 
+void draw(const Color&  color)
+{
+	printf("r:%02x g:%02x b:%02x a:%02x", color.RGBA.r, color.RGBA.g, color.RGBA.b, color.RGBA.alpha);
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Color color = std::move("0x12345678"_argb);
+	Color color = 0xFFEEBBDD_argb;
+	printf("r:%02x g:%02x b:%02x a:%02x\n", color.RGBA.r, color.RGBA.g, color.RGBA.b, color.RGBA.alpha);
 
+	color = "#FEBBDDAA"_argb;
 	printf("r:%02x g:%02x b:%02x a:%02x", color.RGBA.r, color.RGBA.g, color.RGBA.b, color.RGBA.alpha);
+
+	draw(0xFFEEBBDD_argb);
+
 	getchar();
 	return 0;
 }
